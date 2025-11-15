811d99fd286b1995e308ed114f348e0c5c508abf
Noushin Ibrahim <noshi7068@gmail.com>
2025-11-15 15:28:14 +0500

    create reactapp with vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Fn1 from "./Func"
+
+function App() {
+  
+
+  return (
+    <>
+      <h1> | react with vite |</h1>
+      <Fn1 />
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Func.jsx
@@ -0,0 +1,7 @@
+function Fn1(){
+    return(
+        <h3>this is 2nd component</h3>
+    )
+}
+
+export default Fn1
