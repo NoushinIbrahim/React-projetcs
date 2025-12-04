085759cd87f5a0ccadf81f243248ddf82ce817c0
Noushin Ibrahim <noshi7068@gmail.com>
2025-12-04 15:24:10 +0500

    updated code in reduxtoolkittodo

--- reduxToolKitTodo/src/compnents/Addtodo.jsx
+++ reduxToolKitTodo/src/compnents/Addtodo.jsx
@@ -3,23 +3,23 @@ import {useDispatch} from 'react-redux'
 import {addTodo} from '../features/todo/todoslice'
 
 function Addtodo() {
-    const [input, setinput] = useState('')
+    const [input, setInput] = useState('')
     const dispatch = useDispatch()
 
-    const addTodohandler = (e) =>{
-        e.preventDefault()
-        dispatch(addTodo(input))
-        setinput('')
-    }
+    const addTodoHandler = (e) => {
+      e.preventDefault();
+      dispatch(addTodo(input));
+      setInput("");
+    };
 
   return (
-    <form onSubmit={addTodohandler} className="space-x-3 mt-12">
+    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
       <input
         type="text"
-        className="bg-gray-800 border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors durantion-200 ease-in-out"
-        placeholder="Enter a Todo"
+        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        placeholder="Enter a Todo..."
         value={input}
-        onChange={(e) => setinput(e.target.value)}
+        onChange={(e) => setInput(e.target.value)}
       />
       <button
         type="submit"
