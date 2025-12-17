440869fe7bf5ae44c1a83a385c508153dffce17a
Noushin Ibrahim <noshi7068@gmail.com>
2025-12-17 21:29:31 +0500

    updated code

--- Ecommerce-Website/src/App.jsx
+++ Ecommerce-Website/src/App.jsx
@@ -18,7 +18,7 @@ import Payment from './Pages/payment/Payment.jsx'
 function App() {
   return (
     <>
-      <BrowserRouter>
+      {/* <BrowserRouter basename="/React-projetcs/Ecommerce-Website"> */}
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -32,7 +32,7 @@ function App() {
           <Route path="/contact" element={<ContactComponent />} />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
     </>
   );
 }
--- Ecommerce-Website/vite.config.js
+++ Ecommerce-Website/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/React-projetcs/Ecommerce-Website/",
+  base: "/React-projetcs/Ecommerce-Website/", // Important for GitHub Pages subfolder
 });
